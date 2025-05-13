68c200a363b4dfa68e7a03f714fad8cd2f31eb3c
Karen <mingki080808@gmail.com>
2025-05-13 13:08:11 +0900

    Initial commit for poppop project

---
+++ src/main/java/db2025/poppop/DataBaseClass2025Application.java
@@ -0,0 +1,13 @@
+package db2025.poppop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseClass2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseClass2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/DataBaseClass2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package db.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseClass2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
